9181954ae1a4969bafdf05caaffcb34088da7118
EDWIN AU <edwinau@rogers.com>
2017-05-03 16:05:47 -0400

    part 1 completion

--- election.js
+++ election.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 
   // Imagination!
 
+    $.ajax({
+      url: 'bb-election-api.herokuapp.com',
+      method: 'GET'
+      dataType: 'json'
+    }).done(function(data) {
+      for (var i=0; i < data.candidates.length; i++) {
+        candidate = data.candidate[i]
+        $('<li>').html(candidate.name + ' has ' + candidate.votes + ' votes.').appendTo('#everyCandidate');
+      }
+  });
 });
